6450b7771e395d2af71e74aa315ff86729310c98
Hadi <dboukhadi7@gmail.com>
2025-10-26 23:34:34 +0200

    added startup html

--- app/main.py
+++ app/main.py
@@ -2,9 +2,15 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from pathlib import Path
+from starlette.requests import Request
 
-from app.api.files import router as files_router  # <-- add this
+from app.api.files import router as files_router  # router for files
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+templates = Jinja2Templates(directory=str(BASE_DIR / "frontend" / "templates"))
 app = FastAPI(
     title="Parse – Stage 1 (API)",
     version="0.1.0",
@@ -20,13 +26,19 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+app.mount(
+    "/static",
+    StaticFiles(directory=str(BASE_DIR / "frontend" / "static")),
+    name="static",
+)
+
 @app.get("/health", tags=["meta"])
 def health():
     return {"status": "ok"}
 
 @app.get("/", include_in_schema=False)
-def root_to_docs():
-    return RedirectResponse(url="/docs")
+async def show_home(request: Request):
+    return templates.TemplateResponse("right_home.html", {"request": request})
 
 # register routes
 app.include_router(files_router)
